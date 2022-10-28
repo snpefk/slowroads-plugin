bb9ce3dedf8fbf0f9824aa1c3186dab6428b5bd3
Snpefk <snpefk@gmail.com>
2022-10-29 00:22:37 +0400

    Update gradle settings

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
-    // Apply the Java Gradle plugin development plugin to add support for developing Gradle plugins
     `java-gradle-plugin`
-
-    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    `maven-publish`
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("slowroads-plugin") version "1.0"
 }
 
-repositories {
-    mavenCentral()
-}
+group = "io.github.snpefk"
+version = "1.0"
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
@@ -16,45 +14,11 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest()
-        }
-
-        // Create a new test suite
-        val functionalTest by registering(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest()
-
-            dependencies {
-                // functionalTest test suite depends on the production code in tests
-                implementation(project)
-            }
-
-            targets {
-                all {
-                    // This test suite should run after the built-in test suite has run its tests
-                    testTask.configure { shouldRunAfter(test) } 
-                }
-            }
-        }
-    }
-}
-
 gradlePlugin {
-    // Define the plugin
-    val greeting by plugins.creating {
-        id = "io.github.snpefk.greeting"
-        implementationClass = "io.github.snpefk.FreeridePlugin"
+    plugins.create("slowroads") {
+        id = "slowroads-plugin"
+        displayName = "SlowRoads Plugin"
+        description = "The worst thing you can add to your build"
+        implementationClass = "io.github.snpefk.SlowRoadsPlugin"
     }
-}
-
-gradlePlugin.testSourceSets(sourceSets["functionalTest"])
-
-tasks.named<Task>("check") {
-    // Include functionalTest as part of the check lifecycle
-    dependsOn(testing.suites.named("functionalTest"))
 }
